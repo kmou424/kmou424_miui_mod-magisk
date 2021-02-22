923740407db8e7aa9c69ed2f17c7f25ab8ac1ba3
kmou424 <dx2689282850@gmail.com>
2021-02-22 13:42:52 +0800

    pack: Auto generate module.prop
    Signed-off-by: kmou424 <dx2689282850@gmail.com>

--- pack.sh
+++ pack.sh
@@ -1,1 +1,19 @@
-zip -r -0 release.zip * -x "pack.sh" ".gitignore"
+DATE=`date --date='0 days ago' "+%Y-%m-%d"`
+TIME=`date --date='0 days ago' "+%H:%M:%S"`
+
+DATE2=`date --date='0 days ago' "+%Y%m%d"`
+TIME2=`date --date='0 days ago' "+%H%M%S"`
+
+HOST=`hostname`
+ME=`whoami`
+
+echo "id=kmou424_miui_mod
+name=kmou424 MIUI Mod
+version=$DATE $TIME
+versionCode=1
+author=kmou424
+description=kmou424的自用功能性集合Magisk模块 for MIUI。 Build on $HOST@$ME" > module.prop
+
+zip -r -0 kmou424_MIUI_mod-$DATE2-$TIME2.zip * -x "pack.sh" ".gitignore"
+
+rm -rf module.prop
