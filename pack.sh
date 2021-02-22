ac9f60f0cd816bf9c68c09d4cfed6d3a5beead51
kmou424 <dx2689282850@gmail.com>
2021-02-22 13:47:20 +0800

    pack: Make release filename looking better
    Signed-off-by: kmou424 <dx2689282850@gmail.com>

--- pack.sh
+++ pack.sh
@@ -2,7 +2,7 @@ DATE=`date --date='0 days ago' "+%Y-%m-%d"`
 TIME=`date --date='0 days ago' "+%H:%M:%S"`
 
 DATE2=`date --date='0 days ago' "+%Y%m%d"`
-TIME2=`date --date='0 days ago' "+%H%M%S"`
+TIME2=`date --date='0 days ago' "+%H-%M-%S"`
 
 HOST=`hostname`
 ME=`whoami`
@@ -14,6 +14,6 @@ versionCode=1
 author=kmou424
 description=kmou424的自用功能性集合Magisk模块 for MIUI。 Build on $HOST@$ME" > module.prop
 
-zip -r -0 kmou424_MIUI_mod-$DATE2-$TIME2.zip * -x "pack.sh" ".gitignore" "*.zip"
+zip -r -0 kmou424_MIUI_mod_$DATE2_$TIME2.zip * -x "pack.sh" ".gitignore" "*.zip"
 
 rm -rf module.prop
