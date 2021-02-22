533d7383847292a6ad14fc905dd6325d4580758c
kmou424 <dx2689282850@gmail.com>
2021-02-22 20:19:46 +0800

    pack: Add subversion and remove time in filename
    Signed-off-by: kmou424 <dx2689282850@gmail.com>

--- pack.sh
+++ pack.sh
@@ -1,4 +1,5 @@
 VERSION=0.1-Alpha
+SUBVERSION=-debug
 
 if [ ! -d "old" ]; then
 	mkdir old
@@ -6,21 +7,21 @@ fi
 
 mv *.zip old/
 
-DATE=`date --date='0 days ago' "+%Y-%m-%d"`
-TIME=`date --date='0 days ago' "+%H:%M:%S"`
-
-DATE2=`date --date='0 days ago' "+%Y%m%d"`
-
+DATE=`date --date='0 days ago' "+%Y%m%d"`
 HOST=`hostname`
 ME=`whoami`
 
+if [ $1 == "release" ];then
+SUBVERSION=-release
+fi
+
 echo "id=kmou424_miui_mod
 name=kmou424 MIUI Mod
-version=${VERSION}(${DATE} ${TIME})
+version=${VERSION}${SUBVERSION}(${DATE})
 versionCode=1
 author=kmou424
 description=kmou424的自用功能性集合Magisk模块 for MIUI。 Build on $HOST@$ME" > module.prop
 
-zip -r -0 kmou424_MIUI_mod_${VERSION}_${DATE2}.zip * -x "pack.sh" ".gitignore" "*.zip" "*.bak" "old/"
+zip -r -0 kmou424_MIUI_mod_${VERSION}${SUBVERSION}_${DATE}.zip * -x "pack.sh" ".gitignore" "*.zip" "*.bak" "old/"
 
 rm -rf module.prop
