bd5651c5e001f19d9ef416c824fa7f2ea56f8f6b
kmou424 <dx2689282850@gmail.com>
2021-02-22 20:13:04 +0800

    pack: Add version to filename
    Signed-off-by: kmou424 <dx2689282850@gmail.com>

--- pack.sh
+++ pack.sh
@@ -1,3 +1,5 @@
+VERSION=0.1-Alpha
+
 if [ ! -d "old" ]; then
 	mkdir old
 fi
@@ -8,18 +10,17 @@ DATE=`date --date='0 days ago' "+%Y-%m-%d"`
 TIME=`date --date='0 days ago' "+%H:%M:%S"`
 
 DATE2=`date --date='0 days ago' "+%Y%m%d"`
-TIME2=`date --date='0 days ago' "+%H-%M-%S"`
 
 HOST=`hostname`
 ME=`whoami`
 
 echo "id=kmou424_miui_mod
 name=kmou424 MIUI Mod
-version=$DATE $TIME
+version=${VERSION}(${DATE} ${TIME})
 versionCode=1
 author=kmou424
 description=kmou424的自用功能性集合Magisk模块 for MIUI。 Build on $HOST@$ME" > module.prop
 
-zip -r -0 kmou424_MIUI_mod_$DATE2_$TIME2.zip * -x "pack.sh" ".gitignore" "*.zip" "*.bak" "old/"
+zip -r -0 kmou424_MIUI_mod_${VERSION}_${DATE2}.zip * -x "pack.sh" ".gitignore" "*.zip" "*.bak" "old/"
 
 rm -rf module.prop
