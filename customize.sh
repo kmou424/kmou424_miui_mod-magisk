c639ccb56bee1e2d507b40670e31ce1edb61ea50
kmou424 <dx2689282850@gmail.com>
2021-02-23 18:11:51 +0800

    treewide: unzip: Silence
    Signed-off-by: kmou424 <dx2689282850@gmail.com>

--- customize.sh
+++ customize.sh
@@ -8,7 +8,7 @@ UTILSPATH=/data/kmou424
 
 rm -rf $UTILSPATH
 mkdir -p $UTILSPATH
-unzip -o "$ZIPFILE" 'assets/*' -d $UTILSPATH
+unzip -q -o "$ZIPFILE" 'assets/*' -d $UTILSPATH
 set_perm_recursive  $UTILSPATH  0  0  0755  0644
 
 . $UTILSPATH/assets/bin/miui_feature_patcher.sh
